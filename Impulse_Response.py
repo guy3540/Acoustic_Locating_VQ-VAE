4a4a31a0c728fafed829a62dad28d3dda2e8ed8f
Guy <guy3540@gmail.com>
2024-03-24 11:42:18 +0200

    Add STFT to the Impulse Response example

--- Impulse_Response.py
+++ Impulse_Response.py
@@ -8,6 +8,8 @@ import os
 import urllib.request
 import tarfile
 
+from scipy import signal
+
 
 data_dir = 'data/'
 data_url = 'https://www.openslr.org/resources/12/dev-clean.tar.gz'
@@ -22,14 +24,13 @@ if not os.path.isfile(filepath):
     downloaded.close()
 
 orig_signal, fs = sf.read("data/LibriSpeech/dev-clean/84/121123/84-121123-0000.flac", always_2d=True)
+f, t, Zxx = signal.stft(np.squeeze(orig_signal), fs)
 
 h = rir.generate(
     c=340,                  # Sound velocity (m/s)
     fs=fs,                  # Sample frequency (samples/s)
     r=[                     # Receiver position(s) [x y z] (m)
-        [2, 1.5, 1],
-        [2, 1.5, 2],
-        [2, 1.5, 3]
+        [2, 1.5, 1]
     ],
     s=[2, 3.5, 2],          # Source position [x y z] (m)
     L=[5, 4, 6],            # Room dimensions [x y z] (m)
@@ -47,11 +48,19 @@ print(signal.shape)         # (15557, 2, 3)
 
 fig, axes = plt.subplots(3)
 axes[0].plot(orig_signal)
-axes[0].title.set_text('Output signal')
+axes[0].title.set_text('Input signal')
+
 axes[1].plot(h)
 axes[1].title.set_text('Room Impulse Response')
 axes[2].plot(signal[:, 0, :])
 axes[2].title.set_text('Output signal')
 
 plt.tight_layout()  # Fix subplot spacing
+plt.show()
+
+amp = 2 * np.sqrt(2)
+plt.pcolormesh(t, f, np.abs(Zxx), vmin=np.abs(Zxx).min(), vmax=np.abs(Zxx).max())
+plt.title('STFT Magnitude, original signal')
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
 plt.show()
